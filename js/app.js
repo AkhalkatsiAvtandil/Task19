c9c424a0f5bdf64ca547d4f5a7a2831e0670dec3
AkhalkatsiAvtandil <akhalkatsiavto@gmail.com>
2026-05-18 18:14:40 +0400

    task2

--- js/app.js
+++ js/app.js
@@ -8,3 +8,11 @@ button.addEventListener("click", () => {
   console.log("button clicked");
   button.remove();
 });
+
+// task2
+const newimg = document.createElement("img");
+newimg.src =
+  "https://fastly.picsum.photos/id/534/1000/800.jpg?hmac=tFbU1nZ2RnQNroI_ToBhH-LFB8KNjyWoc3bVv5G9Wkw";
+newimg.alt = "table";
+const section = document.querySelector("section");
+section.appendChild(newimg);
